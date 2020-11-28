0516caab2d319d8133d0a51aade1bb1f8a03f09c
Utku Güler <utkuguler41@gmail.com>
2020-11-28 15:50:30 +0300

    Proje dosyası ekle.

---
+++ 06/musteri.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _06
+{
+    class Musteri {
+
+
+     
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Öğrencimisiniz?   (Evet/Hayır)");
+            string cevap = Console.ReadLine();
+            if (cevap == "Evet")
+            {
+                ogrenci ogrenci = new ogrenci();
+                ogrenci.ad = "Utku";
+                ogrenci.soyad = "Güler";
+                ogrenci.ogrno = 221903550;
+                ogrenci.dogumyili = 2000;
+                ogrenci.Yas = 20;
+
+                Console.WriteLine(ogrenci.ad);
+                Console.WriteLine(ogrenci.soyad);
+                Console.WriteLine(ogrenci.ogrno);
+                Console.WriteLine(ogrenci.dogumyili);
+                Console.WriteLine(ogrenci.Yas);
+
+            }
+            else if (cevap == "Hayır")
+            {
+                ogrenci musteri = new ogrenci();
+                musteri.ad = "Erdem";
+                musteri.soyad = "Endeş";
+                musteri.dogumyili = 1980;
+                musteri.meslek = "Endüstri Mühendisi";
+                musteri.maas = 25000;
+                musteri.Yas = 40;
+
+
+                Console.WriteLine(musteri.ad);
+                Console.WriteLine(musteri.soyad);
+                Console.WriteLine(musteri.dogumyili);
+                Console.WriteLine(musteri.meslek);
+                Console.WriteLine(musteri.maas);
+                Console.WriteLine(musteri.Yas);
+
+
+
+            }
+
+
+
+        }
+    }
+}
---
+++ 06/ogrenci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06
+{
+    class ogrenci
+    {
+        public string ad;
+        public string soyad;
+        public int ogrno;
+        public int maas;
+        public string meslek;
+        public int dogumyili;
+        public int Yas;
+        
+       
+    }
+}
